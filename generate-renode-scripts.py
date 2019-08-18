e2f6a0058120b5472a950efe81cc0811104f19a5
Carl Karsten <carl@personnelware.com>
2019-08-17 23:03:56 -0500

    adds --json-file and code to create a json file for the moc server.

--- generate-renode-scripts.py
+++ generate-renode-scripts.py
@@ -12,6 +12,8 @@ import sys
 import csv
 import zlib
 import argparse
+import collections
+import json
 
 # those memory regions are handled in a special way
 # and should not be generated automatically
@@ -19,6 +21,7 @@ non_generated_mem_regions = ['ethmac', 'spiflash', 'csr']
 
 mem_regions = {}
 peripherals = {}
+registers = {}
 constants = {}
 
 
@@ -403,8 +406,12 @@ def parse_csv(data):
             # CSRs have already been parsed
             pass
         elif _type == 'csr_register':
-            # we are currently not interested in this
-            pass
+            # csr_register,info_dna_id,0xe0006800,8,ro
+            registers[_name] = {'name': _name,
+                               'address': _val,
+                               'size': _val2,
+                               'r': _
+                               }
         elif _type == 'constant':
             found = False
             for _csr_name in peripherals:
@@ -538,6 +545,79 @@ def remove_comments(data):
             yield line
 
 
+def mk_obj():
+    """
+    Makes an object of dicts and lists from the registers var
+
+    """
+
+    def make_dict():
+        return collections.defaultdict(make_dict);
+
+    def set_path(d, path, value):
+        if path[-1] in ['reset', 'issue', 'en']: return
+        for key in path[:-1]:
+            d = d[key]
+        d[path[-1]] = value
+
+    the_dict = make_dict()
+    for register in registers:
+        set_path(
+                the_dict,
+                register.split('_'),
+                registers[register],
+                )
+
+    return the_dict
+
+def crawl(d):
+
+    # normalize
+    # look for in1:v in2:v and change to in:[v,v]
+
+    # are we on a leaf?
+    if not hasattr(d,'keys'): return
+
+    # look for fooN keys
+    keys=[]
+    for k in d:
+        crawl(d[k])
+        if k in ['fx2']: continue # special cases: fx2 is not a 0,1,2
+        if k[-1].isdigit():
+            keys.append(k)
+
+    # consolodate them into foo[0,1,2]
+    keys.sort()
+    for k in keys:
+        # grab the value and remove the fooN item
+        v=d.pop(k)
+        # split into foo and N
+        k,n=k[:-1],k[-1]
+        # make a foo list
+        if n=='0':
+            d[k]=[]
+        # append all the values to the foo list
+        d[k].append(v)
+
+
+def mk_json(filepath):
+    """ writes json file for the moc server to serve.
+
+    Args:
+        filepath (string): path to the file lines should be written to
+                           or '-' to write to a standard output
+
+    the module's registers var data is turned into an object
+    and serialized to a .json file.
+    """
+
+    o = mk_obj()
+    crawl(o)
+
+    with open(filepath, 'w') as f:
+        json.dump(o, f, indent=2)
+
+
 def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument('conf_file',
@@ -552,6 +632,8 @@ def parse_args():
                         help='Path to the BIOS binary')
     parser.add_argument('--firmware-binary', action='store',
                         help='Path to the binary to load into boot flash')
+    parser.add_argument('--json-file', action='store',
+                        help='Output json file for moc server')
     args = parser.parse_args()
 
     return args
@@ -574,6 +656,9 @@ def main():
                                                    args.configure_network,
                                                    args.bios_binary,
                                                    args.firmware_binary))
+
+    if args.json_file:
+        mk_json(args.json_file)
 
 
 if __name__ == '__main__':
